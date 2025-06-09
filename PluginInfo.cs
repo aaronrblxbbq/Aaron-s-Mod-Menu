a1993e4359bfea268025168b0dccfe339262b097
Aaronrblxbbq <124796861+aaronrblxbbq@users.noreply.github.com>
2025-06-09 08:01:37 +0200

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -2,9 +2,9 @@
 {
     internal class PluginInfo
     {
-        public const string GUID = "org.iidk.gorillatag.menutemplate";
-        public const string Name = "ii's Stupid Template";
-        public const string Description = "Created by @goldentrophy with love <3";
+        public const string GUID = "aar.mod.menu.Privat.use";
+        public const string Name = "Aarons Mode menu";
+        public const string Description = "Made by AaronSeesClouds, and template made by iiDk";
         public const string Version = "1.0.0";
     }
 }
